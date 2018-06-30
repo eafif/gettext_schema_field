4ffe8ca963a2bd2ef3ee18ec3b8dd263d3883cfc
eafif <healthswappdev@gmail.com>
2018-06-30 15:20:22 -0400

    initial commit

---
+++ lib/gettext_schema_field.ex
@@ -0,0 +1,18 @@
+defmodule GettextSchemaField do
+  @moduledoc """
+  Documentation for GettextSchemaField.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GettextSchemaField.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gettext_schema_field_test.exs
@@ -0,0 +1,8 @@
+defmodule GettextSchemaFieldTest do
+  use ExUnit.Case
+  doctest GettextSchemaField
+
+  test "greets the world" do
+    assert GettextSchemaField.hello() == :world
+  end
+end
