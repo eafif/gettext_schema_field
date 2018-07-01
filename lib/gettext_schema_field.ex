96c93618aca4b38d4d6d0b34ccbe7b1db9d9774d
eafif <healthswappdev@gmail.com>
2018-06-30 23:30:32 -0400

    helper module to translate schema fields

--- lib/gettext_schema_field.ex
+++ lib/gettext_schema_field.ex
@@ -1,18 +1,31 @@
 defmodule GettextSchemaField do
   @moduledoc """
-  Documentation for GettextSchemaField.
+  This module provides a helper method `translate_schema_field` to translate a changeset field
+  based on PO files
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> GettextSchemaField.hello
-      :world
-
+  Returns the translation of the field. If the msgstr is missing 
+  in the PO file, the original field is returned
   """
-  def hello do
-    :world
+  def translate_schema_field(gettext_module, changeset, field) do
+    msgid = "#{module_name(changeset)}.#{field}"
+
+    Gettext.dgettext(gettext_module, schema_fields_domain(), msgid, [])
+    |> sanitize_result(msgid, field)
   end
+
+  defp module_name(changeset), do: sanitized_module_name("#{changeset.data.__struct__}")
+
+  defp sanitized_module_name("Elixir." <> rest), do: rest
+  defp sanitized_module_name(module_name), do: module_name
+
+  defp schema_fields_domain do
+    Application.get_env(Mix.Project.config()[:app], :gettext_schema_file_name) || "schema"
+  end
+
+  # If the msgstr and the msgid are the same, assume that the translation is missing
+  # and return the original field
+  defp sanitize_result(msgid, msgid, field), do: field
+  defp sanitize_result(msgstr, _msgid, _field), do: msgstr
 end
--- test/gettext_schema_field_test.exs
+++ test/gettext_schema_field_test.exs
@@ -2,7 +2,4 @@ defmodule GettextSchemaFieldTest do
   use ExUnit.Case
   doctest GettextSchemaField
 
-  test "greets the world" do
-    assert GettextSchemaField.hello() == :world
-  end
 end
